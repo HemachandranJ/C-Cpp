1d659ccbf82e0ae9aa189985b1123bb94c375a61
HemachandranJ <hemachandran.j@tataelxsi.co.in>
2021-02-08 18:16:49 +0530

    add main.exe

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+extern "C" {
+void f();
+}
+
+void func(void)
+{
+    std::cout<<"\n being used within C++ code\n";
+}
+
+int main(void)
+{
+    f();
+    func();
+    return 0;
+}
